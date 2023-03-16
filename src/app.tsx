bf0d4ce961e492ba3fb1ed664f1e80ed84ba4424
Gauthier Boddaert <boddaert.gauthier@gmail.com>
2023-03-16 22:45:18 +0100

    add pokemon

--- src/app.tsx
+++ src/app.tsx
@@ -1,10 +1,15 @@
-import React, {FunctionComponent} from "react";
+import React, {FunctionComponent, useState} from "react";
+import Pokemon from "./models/Pokemon";
+import MockPokemon from "./models/mock-pokemon";
 
 const App: FunctionComponent = () => {
-    const name : String = 'React';
+
+    const [name, setState] = useState<String>('react');
+    const [pokemon, setPokemon] = useState<Array<Pokemon>>(MockPokemon)
 
     return (
-        <h1>Bonjour, {name} ! </h1>
+        <h1>Il y a {pokemon.length} présent dans le pokedex </h1>
+
     )
 }
 
